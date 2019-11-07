6a2a023889a01867418b27368c7a08c91b3cdbed
Stephaniesandra <stephanie@taxbol.dk>
2019-11-07 12:38:04 +0100

    back to top jQuery dokument til index side.

---
+++ js/backtotop.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop()) {
+            $('.toTop').fadeIn();
+        } else {
+            $('.toTop').fadeOut();
+        }
+
+    });
+
+    $('.toTop').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1000);
+        return false;
+
+    });
+
+});
